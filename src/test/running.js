0c3c6bfd4538eacdc2a0cbafbfb0371763bf07b2
Lars Olesen <lars@intraface.dk>
2019-12-08 07:25:50 +0000

    Added converters

--- src/test/running.js
+++ src/test/running.js
@@ -23,4 +23,18 @@ describe('RunningTest', function() {
       assert.equal(r.getTimePrKilometer(12, 0, 3), "4:00");
     });
   });
+  describe('convertKmtToMinPerKm', function() {
+    it('should return the correct number', function() {
+      var r = motionsplan.Running();
+      // m2, s2, km
+      assert.equal(r.convertKmtToMinPerKm(12), "5:00");
+    });
+  });
+  describe('convertMinPerKmToKmt', function() {
+    it('should return the correct number', function() {
+      var r = motionsplan.Running();
+      // m2, s2, km
+      assert.equal(r.convertMinPerKmToKmt(4, 0), 15);
+    });
+  });
 });
