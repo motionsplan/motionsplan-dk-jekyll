57031e20f9674ca1234b7970c8e61ea542c71898
Lars Olesen <lars@intraface.dk>
2020-02-11 15:42:14 +0000

    Fixes to Durnin

--- src/js/skinfold-durnin.js
+++ src/js/skinfold-durnin.js
@@ -11,16 +11,48 @@ motionsplan.SkinfoldDurnin = function(biceps, triceps, hoftekam, skulder, weight
   age = age;
 
   function getBodyFatPercent() {
+    return (495 / getDensity() - 450);
+  }
+
+  function getFatSum() {
+    return biceps * 1 + triceps * 1 + hoftekam * 1 + skulder * 1;
+  }
+
+  function getDensity() {
     var density;
 
-    var fedtsum = biceps * 1 + triceps * 1 + hoftekam * 1 + skulder * 1;
+    var fedtsum = getFatSum();
     if (isMale()) {
-      density = -0.0274 * Math.log(fedtsum) + 1.1631;
+      if (age < 17) {
+        density = 1.1533 - 0.0643 * Math.log10(fedtsum);
+      } else if (age < 19) {
+        density = 1.1620 - 0.0630 * Math.log10(fedtsum);
+      } else if (age < 29) {
+        density = 1.1631 - 0.0632 * Math.log10(fedtsum);
+      } else if (age < 39) {
+        density = 1.1422 - 0.0544 * Math.log10(fedtsum);
+      } else if (age < 49) {
+        density = 1.1620 - 0.0700 * Math.log10(fedtsum);
+      } else {
+        density = 1.1715 - 0.0779 * Math.log10(fedtsum);
+      }
     }
     else {
-      density = -0.0311 * Math.log(fedtsum) + 1.1599;
+      if (age < 17) {
+        density = 1.1369 - 0.0598 * Math.log10(fedtsum);
+      } else if (age < 19) {
+        density = 1.1549 - 0.0678 * Math.log10(fedtsum);
+      } else if (age < 29) {
+        density = 1.1599 - 0.0717 * Math.log10(fedtsum);
+      } else if (age < 39) {
+        density = 1.1423 - 0.0632 * Math.log10(fedtsum);
+      } else if (age < 49) {
+        density = 1.1333 - 0.0612 * Math.log10(fedtsum);
+      } else {
+        density = 1.1339 - 0.0645 * Math.log10(fedtsum);
+      }
     }
-    return (495 / density - 450) * Math.pow(10, 1) / Math.pow(10, 1);
+    return density; 
   }
   
   function isMale() {
@@ -36,7 +68,9 @@ motionsplan.SkinfoldDurnin = function(biceps, triceps, hoftekam, skulder, weight
 
   var publicAPI = {
     getFatFreeMass: getFatFreeMass,
-    getBodyFatPercent: getBodyFatPercent
+    getBodyFatPercent: getBodyFatPercent,
+    getDensity : getDensity,
+    getFatSum : getFatSum
   };
 
   return publicAPI;
--- src/test/durnin.js
+++ src/test/durnin.js
@@ -7,8 +7,8 @@ describe('SkinfoldDurnin', function() {
 
       // biceps, triceps, hoftekam, skulder, weight, gender, age
       var hr = motionsplan.SkinfoldDurnin(10, 10, 20, 20, 80, "male");
-      assert.equal(hr.getBodyFatPercent(), 21.01813116574084);
-      assert.equal(hr.getFatFreeMass(), 63.18549506740733);
+      assert.equal(hr.getBodyFatPercent(), 21.10515057483525);
+      assert.equal(hr.getFatFreeMass(), 63.1158795401318);
     });
   });
   describe('getBodyPercentFat()', function() {
@@ -16,8 +16,23 @@ describe('SkinfoldDurnin', function() {
 
       // biceps, triceps, hoftekam, skulder, weight, gender, age
       var hr = motionsplan.SkinfoldDurnin(10, 10, 20, 20, 80, "female");
-      assert.equal(hr.getBodyFatPercent(), 29.388296296763826);
-      assert.equal(hr.getFatFreeMass(), 56.48936296258894);
+      assert.equal(hr.getBodyFatPercent(), 29.462279114830324);
+      assert.equal(hr.getFatFreeMass(), 56.43017670813574);
+
+      var hr = motionsplan.SkinfoldDurnin(10, 10, 20, 20, 80, "female", 60);
+      assert.equal(hr.getBodyFatPercent(), 35.67063411553147);
+      assert.equal(hr.getFatFreeMass(), 51.46349270757482);
     });
   });
+  describe('getDensity()', function() {
+    it('should return the correct number', function() {
+
+      // biceps, triceps, hoftekam, skulder, weight, gender, age
+      // https://www.thecalculator.co/health/Body-Fat-Skinfold-By-Durnin-And-Womersley-Calculator-1111.html
+      var hr = motionsplan.SkinfoldDurnin(18, 20, 22, 32, 89, "female", 30);
+      assert.equal(hr.getFatSum(), 92);
+      assert.equal(hr.getDensity(), 1.018188609311761);
+    });
+  });
+  
 });
