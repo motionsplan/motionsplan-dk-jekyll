49fc7b4ef36ebd7baad24ee0924319b834cb5362
Lars Olesen <lars@intraface.dk>
2021-11-27 12:55:41 +0100

    Trying to fix Youtube again

--- src/js/app.bundle.js
+++ src/js/app.bundle.js
@@ -64,30 +64,27 @@ const hr_intensity = require('../js/hr-intensity');
 const wilks = require('wilks-calculator');
 require('image-map-resizer');
 
-  function labnolThumb(id) {
-    return '<img class="youtube-thumb" src="//i.ytimg.com/vi/' + id + '/hqdefault.jpg"><div class="play-button"></div>';
-  }
-
-  function labnolIframe() {
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "//www.youtube-nocookie.com/embed/" +
-        this.parentNode.dataset.id + "?  autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=0&showinfo=0");
-    iframe.setAttribute("frameborder", "0");
-    iframe.setAttribute("allowfullscreen", "allowfullscreen");
-    iframe.setAttribute("id", "youtube-iframe");
-    this.parentNode.replaceChild(iframe, this);
-  }
-
 $(function() {
     $('map').imageMapResize();
 
-    var v = document.getElementsByClassName("youtube-player");
-    for (var n = 0; n < v.length; n++) {
-        var p = document.createElement("div");
-        p.innerHTML = labnolThumb(v[n].dataset.id);
-        p.onclick = labnolIframe;
-        v[n].appendChild(p);
-    }
+    $(".youtube-player a").each(function() {
+        console.log("Looping through Youtube");
+        $(this).click(function(e) {
+            console.log("Clicked youtube video");
+            e.preventDefault();
+            var iframe = document.createElement("iframe");
+            iframe.setAttribute("src", "//www.youtube-nocookie.com/embed/" +
+                this.parentNode.dataset.id + "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=0&showinfo=0");
+            iframe.setAttribute("frameborder", "0");
+            iframe.setAttribute("allowfullscreen", "allowfullscreen");
+            iframe.setAttribute("id", "youtube-iframe-" + this.parentNode.dataset.id);
+            /*
+            this.parentNode.replaceChild(iframe, this);
+            */
+            $(this).parent().replaceWith(iframe);
+            //$(this).replaceWith(newElem);
+        });
+    });
 
     $("#target_bmi_man").change(function() {
         console.log('Ready to calculate');
