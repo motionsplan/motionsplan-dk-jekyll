482e391ce5cacfab6d4d3a2abbf8ae00f102ac34
Lars Olesen <lars@intraface.dk>
2021-11-30 22:39:41 +0100

    Added more formulas to calculator

--- src/js/app.bundle.js
+++ src/js/app.bundle.js
@@ -831,10 +831,13 @@ $(function() {
         let f = fat.CalculateFatPercent(h, w, a, g);
 
         $("[name='BMI']").val(f.getBMI().toFixed(2));
-        $("[name='fat_percent_heitmann']").val(f.getBodyFatPercentHeitmannBMIEquation().toFixed(1));
-        $("[name='fat_percent_durnin']").val(f.getBodyFatPercentWomersleyDurninBMIEquation().toFixed(1));
-        $("[name='fat_percent_duerenberg']").val(f.getBodyFatPercentDuerenbergBMIEquation().toFixed(1));
-        $("[name='fat_percent_gallagher']").val(f.getBodyFatPercentGallagher().toFixed(1));
+        $("[name='fat_percent_durnin']").val(f.getWomersleyDurnin1977().toFixed(1));
+        $("[name='fat_percent_jackson_pollock']").val(f.getJacksonPollock1980().toFixed(1));
+        $("[name='fat_percent_heitmann']").val(f.getHeitmann1990().toFixed(1));
+        $("[name='fat_percent_duerenberg']").val(f.getDuerenberg1991().toFixed(1));
+        $("[name='fat_percent_duerenberg_1998']").val(f.getDuerenberg1998().toFixed(1));
+        $("[name='fat_percent_gallagher']").val(f.getGallagher2000().toFixed(1));
+        $("[name='fat_percent_heritage_2002']").val(f.getHeritage2002().toFixed(1));
     });
     $("#calculator_how_tall").submit(function(e) {
         console.log("Calculate How Tall");
--- src/test/fat-pct.js
+++ src/test/fat-pct.js
@@ -2,44 +2,80 @@ const motionsplan = require('../js/fat-pct.js');
 var assert = require('assert');
 
 describe('CalculateFatPercent', function() {
-  describe('getFatMass', function() {
+  describe('getHeitmann1990', function() {
     it('should return the correct number', function() {
-
-      // age, heart rate, blood pressure, diabetes, smoker, bmi, fitness
       var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
-      assert.equal(fat.getBodyFatPercentHeitmannBMIEquation(), 21.668827160493827);
+      assert.equal(fat.getHeitmann1990(), 21.668827160493827);
       assert.equal(fat.getFatMass(), 17.33506172839506);
     });
-  });
-  describe('getFatMassFromWormsleyDurninBMIIndexEq', function() {
     it('should return the correct number', function() {
-
-      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
-      assert.equal(fat.getBodyFatPercentWomersleyDurninBMIEquation(), 20.616419753086426);
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'woman');
+      assert.equal(fat.getHeitmann1990(), 31.868827160493822);
+      assert.equal(fat.getFatMass(), 25.495061728395058);
     });
   });
-  describe('getFatPercentDuernebergBMIIndexEq', function() {
+  describe('getWormsleyDurnin1977', function() {
     it('should return the correct number', function() {
       var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
-      assert.equal(fat.getBodyFatPercentDuerenbergBMIEquation(), 22.629629629629626);
+      assert.equal(fat.getWomersleyDurnin1977(), 20.616419753086426);
     });
     it('should return the correct number', function() {
       var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'woman');
-      assert.equal(fat.getBodyFatPercentDuerenbergBMIEquation(), 33.42962962962963);
+      assert.equal(fat.getWomersleyDurnin1977(), 30.357160493827166);
     });
   });
-  describe('getGallagher', function() {
+  describe('getJacksonPollock1980', function() {
     it('should return the correct number', function() {
       var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
-      assert.equal(fat.getBodyFatPercentGallagher(), 21.407999999999998);
-      assert.equal(fat.getBodyFatPercentGallagher("afro"), 20.208);
-      assert.equal(fat.getBodyFatPercentGallagher("asian"), 22.992499999999996);
+      assert.equal(fat.getJacksonPollock1980(), 18.70617283950618);
     });
     it('should return the correct number', function() {
       var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'woman');
-      assert.equal(fat.getBodyFatPercentGallagher(), 33.507999999999996);
-      assert.equal(fat.getBodyFatPercentGallagher("asian"), 35.092499999999994);
-      assert.equal(fat.getBodyFatPercentGallagher("afro"), 32.30799999999999);
+      assert.equal(fat.getJacksonPollock1980(), 30.806172839506182);
+    });
+  });
+  describe('getHeritage2002', function() {
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
+      assert.equal(fat.getHeritage2002(), 21.380987654320986);
+    });
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'woman');
+      assert.equal(fat.getHeritage2002(), 31.720987654320986);
+    });
+  });
+  describe('getDuerenberg1998', function() {
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
+      assert.equal(fat.getDuerenberg1998(), 18.45185185185185);
+    });
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'woman');
+      assert.equal(fat.getDuerenberg1998(), 29.851851851851848);
+    });
+  });
+  describe('getDuerenberg1991', function() {
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
+      assert.equal(fat.getDuerenberg1991(), 22.629629629629626);
+    });
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'woman');
+      assert.equal(fat.getDuerenberg1991(), 33.42962962962963);
+    });
+  });
+  describe('getGallagher2000', function() {
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'man');
+      assert.equal(fat.getGallagher2000(), 21.407999999999998);
+      assert.equal(fat.getGallagher2000("afro"), 20.208);
+      assert.equal(fat.getGallagher2000("asian"), 22.992499999999996);
+    });
+    it('should return the correct number', function() {
+      var fat = motionsplan.CalculateFatPercent(180, 80, 40, 'woman');
+      assert.equal(fat.getGallagher2000(), 33.507999999999996);
+      assert.equal(fat.getGallagher2000("asian"), 35.092499999999994);
+      assert.equal(fat.getGallagher2000("afro"), 32.30799999999999);
     });
   });
 });
