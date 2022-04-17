b9ab3a7e2d3cf47d6435bc7445a9f4e4f2c17f54
Lars Olesen <lars@intraface.dk>
2022-04-17 19:24:21 +0200

    Fixes

--- src/test/strength.js
+++ src/test/strength.js
@@ -12,20 +12,24 @@ describe('Strength', function() {
   describe('getAllometricScaling', function() {
     it('should return the correct number', function() {
       // weigt, reps
-      var rm = motionsplan.Strength(405, 220);
+      let rm = motionsplan.Strength(405, 220);
       assert.equal(rm.getAllometricScaling(), 11.11321976546847);
-      
-      var rm = motionsplan.Strength(300, 150);
+    });
+  });
+  describe('getAllometricScalingSecondTry', function() {
+    it('should return the correct number', function() {
+      let rm = motionsplan.Strength(300, 150);
       assert.equal(rm.getAllometricScaling(), 10.626585691826113);
     });
   });
   describe('getBodyweightPercent', function() {
     it('should return the correct number', function() {
       // weigt, reps
-      var rm = motionsplan.Strength(405, 220);
+      let rm = motionsplan.Strength(405, 220);
       assert.equal(rm.getBodyweightPercent(), 1.8409090909090908);
-      
-      var rm = motionsplan.Strength(300, 150);
+    });
+    it('should return the correct number', function() {      
+      let rm = motionsplan.Strength(300, 150);
       assert.equal(rm.getBodyweightPercent(), 2);
     });
   });
