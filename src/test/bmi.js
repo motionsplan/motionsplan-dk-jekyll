4184fa1af97d5c04e705eef635143c931f61498f
Lars Olesen <lars@intraface.dk>
2022-05-05 09:00:27 +0200

    Fixes

--- src/test/bmi.js
+++ src/test/bmi.js
@@ -8,17 +8,4 @@ describe('BMI', function() {
       assert.equal(hr.getBMI(), 24.691358024691358);
     });
   });
-  describe('getBMI()', function() {
-    it('should return the correct number', function() {
-      var hr = motionsplan.BMI(130, 40);
-      assert.equal(hr.getBMI(), 23.668639053254434);
-      assert.equal(hr.evaluateChildren('female', 6), 'Obese');
-      assert.equal(hr.evaluateChildrenHeight(), 'Your height is above average');
-      assert.equal(hr.evaluateChildren('male', 12), 'Overweight');
-      assert.equal(hr.evaluateChildrenHeight(), 'Your height is below average');
-      assert.equal(hr.evaluateChildren('male', 16), 'Healthy weight');
-      assert.equal(hr.evaluateChildrenHeight(), 'Your height is below average');
->>>>>>> 402f0e2372845e4be695fcf78f838db0b6c1ece7
-    });
-  });
 });
