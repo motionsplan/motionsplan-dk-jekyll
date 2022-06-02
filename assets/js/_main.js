945b01bca8f84106ebc388826070bb967233daa5
Lars Olesen <lars@intraface.dk>
2022-06-02 09:43:52 +0200

    Fixes

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -135,23 +135,4 @@
           $(this).parent().replaceWith(iframe);
       });
     });
-    
-      $('.popup-with-form').magnificPopup({
-        type: 'inline',
-        preloader: false,
-        focus: '#mce-EMAIL',
-
-        // When elemened is focused, some mobile browsers in some cases zoom in
-        // It looks not nice, so we disable it:
-        callbacks: {
-          beforeOpen: function() {
-            if($(window).width() < 700) {
-              this.st.focus = false;
-            } else {
-              this.st.focus = '#mce-EMAIL';
-            }
-          }
-        }
-      });
-    
   });
